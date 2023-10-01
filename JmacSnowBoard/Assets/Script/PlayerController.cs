67266b40c3bcce0f14ca845d7a95dffe79725f78
Jmac4Life <143218795+Jmac4Life@users.noreply.github.com>
2023-09-30 23:08:39 -0400

    Using FindObjectOfType (10:19)

--- JmacSnowBoard/Assets/Script/PlayerController.cs
+++ JmacSnowBoard/Assets/Script/PlayerController.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float fltTorqueAmount = 15f;
+    [SerializeField] float fltTorqueAmount = 10f;
+    [SerializeField] float fltBoostSpeed = 30f;
+    [SerializeField] float fltBaseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)) 
+        {
+            surfaceEffector2D.speed = fltBoostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = fltBaseSpeed;
+        }
+        // If we push up, then speed up
+        // otherwise stay at normal speed
+        // Access the surface effector 2d and change its speed
+    }
+
+    void RotatePlayer()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
