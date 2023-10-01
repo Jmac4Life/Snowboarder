d051e30414f13605f1c146a218db9a0c7e7cf354
Jmac4Life <143218795+Jmac4Life@users.noreply.github.com>
2023-09-30 22:25:01 -0400

    Triggering Particle Effects (7:27)

--- JmacSnowBoard/Assets/Script/CrashDetector.cs
+++ JmacSnowBoard/Assets/Script/CrashDetector.cs
@@ -8,11 +8,13 @@ public class CrashDetector : MonoBehaviour
 {
 
     [SerializeField] float fltLoadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", fltLoadDelay);
         }
     }
